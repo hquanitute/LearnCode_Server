1fc59f2daafcbd9168155c63c6c63ee4856f7d4a
hquanitute <hquanitue@gmail.com>
2020-05-24 12:29:35 +0700

    fix order for query

--- app/routers/courseRouter.js
+++ app/routers/courseRouter.js
@@ -40,7 +40,7 @@ router.post("/", (req, res) => {
     //     }
     //     res.json({ "content": course })
     // })
-    Course.find({}, {}, req.option).populate({path:'lessons',populate:{path:'challenges'}}).exec(
+    Course.find({}, {}, req.option).populate({path:'lessons',populate:{path:'challenges', options: { sort: { 'challengeOrder': 1 }} }, options: { sort: { 'order': 1 } }}).exec(
         (err, course) => {
             if (err) {
                 return res.json({ "status": "error", "value": err });
