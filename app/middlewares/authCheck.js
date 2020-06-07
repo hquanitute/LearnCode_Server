f66ff707ed259cbc8f2d9a991aebf2d2d978bd99
hquanitute <hquanitute@gmail.com>
2020-06-07 14:13:36 +0700

    Fix loi middleware

--- app/middlewares/authCheck.js
+++ app/middlewares/authCheck.js
@@ -1,10 +1,14 @@
 module.exports.authCheck = (req,res,next) => {
     if(process.env.mode =='DEV'){
+        console.log("abc");
         next();
+        //if not use if else if =>>>> after next() must have return
     }
-    if(req.user){
+    else if(req.user){
+        console.log("drf");
         next();
     } else {
-        res.redirect('/');
+        // res.redirect('/');
+        res.status(403).send("Login failed")
     }
 };
