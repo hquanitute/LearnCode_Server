e6f3d6eedfeb5284189915cc4e6990fd122b0507
hquanitute <hquanitute@gmail.com>
2020-07-09 23:01:33 +0700

    Ignore upercase, lowercase for regex

--- app/routers/searchRouter.js
+++ app/routers/searchRouter.js
@@ -5,11 +5,11 @@ var router = express.Router();
 const option = require('./../middlewares/queryOption')
 
 router.get('/',option(), async (req,res) => {
-    let regexp = new RegExp( req.query.key );
+    let regexp = new RegExp( req.query.key, 'i' );
 
     let promiseChallenge = await Challenge.find({"title": regexp} , null, req.option).exec();
     let promiseTopic = await Topic.find({"name": regexp} , null, req.option).exec();
-    
+
     res.status(200).json({
         'challenges': promiseChallenge,
         'topics': promiseTopic
