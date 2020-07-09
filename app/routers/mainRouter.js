f584af83276703ec6bd47d3ed22a351fb35da4f1
hquanitute <hquanitute@gmail.com>
2020-07-09 22:52:01 +0700

    Add api for search

--- app/routers/mainRouter.js
+++ app/routers/mainRouter.js
@@ -6,6 +6,7 @@ const course = require('./courseRouter');
 const lesson = require('./lessonRouter');
 const user = require('./userRouter');
 const topicRouter = require('./topicRouter');
+const searchRouter = require('./searchRouter');
 
 var router = express.Router();
 router.use('/challenges', authCheck, challenge);
@@ -13,5 +14,5 @@ router.use('/courses', authCheck, course);
 router.use('/lessons', authCheck, lesson);
 router.use('/users', authCheck, user);
 router.use('/topics', authCheck, topicRouter);
-
+router.use('/search', searchRouter)
 module.exports = router;
---
+++ app/routers/searchRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const Challenge = require('./../models/challenge');
+const Topic = require('./../models/topic')
+var router = express.Router();
+const option = require('./../middlewares/queryOption')
+
+router.get('/',option(), async (req,res) => {
+    let regexp = new RegExp( req.query.key );
+
+    let promiseChallenge = await Challenge.find({"title": regexp} , null, req.option).exec();
+    let promiseTopic = await Topic.find({"name": regexp} , null, req.option).exec();
+    
+    res.status(200).json({
+        'challenges': promiseChallenge,
+        'topics': promiseTopic
+    });
+    
+})
+
+module.exports = router;
